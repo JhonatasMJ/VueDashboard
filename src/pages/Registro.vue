24ae37875fdeb974eb8562c798b396fa9908d76d
JhonatasMJ <jhonatasjhmj@gmail.com>
2025-04-25 17:56:36 -0300

    Teste

--- src/pages/Registro.vue
+++ src/pages/Registro.vue
@@ -82,7 +82,7 @@ async function handleSubmit() {
         </div>
         <CardHeader className="px-0">
           <CardTitle className="text-4xl text-(--marca) font-bold">
-            Crie sua contaaa
+            Crie sua contaaaa
           </CardTitle>
           <CardDescription className="text-zinc-900 text-md mt-4">
             Crie sua conta para começar a usar seu Dashboard.
